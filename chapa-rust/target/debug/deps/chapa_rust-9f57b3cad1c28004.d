cc255c3a11b9eeb118b428842b27db5ac2533f30
Abreham Kassa <abrehamkassa19@gmail.com>
2025-09-01 18:58:42 +0300

    stop tracking target folder

--- chapa-rust/target/debug/deps/chapa_rust-9f57b3cad1c28004.d
+++
@@ -1,5 +0,0 @@
-c:\Users\leoul\Dropbox\PC (2)\Desktop\chapa\chapa-rust\chapa-rust\target\debug\deps\chapa_rust-9f57b3cad1c28004.rmeta: src\lib.rs
-
-c:\Users\leoul\Dropbox\PC (2)\Desktop\chapa\chapa-rust\chapa-rust\target\debug\deps\chapa_rust-9f57b3cad1c28004.d: src\lib.rs
-
-src\lib.rs:
